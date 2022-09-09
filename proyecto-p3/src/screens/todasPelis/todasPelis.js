f330bcdcf4016f09677834a1404c073b2dd28af3
solanabiblos <sbiblos@udesa.edu.ar>
2022-09-09 19:26:16 -0300

    avances loader en home y boton ver todas

--- proyecto-p3/src/screens/todasPelis/todasPelis.js
+++ proyecto-p3/src/screens/todasPelis/todasPelis.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import verTodasPelis from "../../components/verTodasPelis/verTodasPelis";
+/* import verTodasPelis from "../../components/verTodasPelis/verTodasPelis"; */
 import './todasPelis.css';
 import '../../loadingimg.gif' 
 
