2d2b916ac0086b91a230ab994d6e206788887e08
barralaugusto <augustobarral@gmail.com>
2022-09-11 22:22:15 -0300

    paginado y aplicaciones en home

--- proyecto-p3/src/components/Principal/Principal.js
+++ proyecto-p3/src/components/Principal/Principal.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../MovieCard/MovieCard';
+import { Link } from 'react-router-dom';
+import '../Principal/Principal.css'
 
 class Principal extends Component {
     constructor() {
@@ -9,29 +11,36 @@ class Principal extends Component {
             popularMovies: [],
             cartelMovies: [],
             resultados: [],
+            movies: [],
+            moviesCar: [],
             valor: '',
             verMas: true,
+            mensaje: "Ver más"
         }
     }
 
-    componentDidMount() {      
+    componentDidMount() {
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + this.state.key + '&language=es&page=1')
             .then(data => data.json())
             .then(info => {
                 this.setState({
                     allMovies: info.results,
-                    popularMovies: info.results.slice(0, 10),
-                })
+                    movies: [info.results.slice(0, 5), info.results.slice(5, 10), info.results.slice(10, 15), info.results.slice(15, 20)],
+                    popularMovies: info.results.slice(0, 5),
+                    index: 0,
+                }, ()=> console.log(this.state.movies[0]))
             })
 
         fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=' + this.state.key)
             .then(data => data.json())
             .then(info => {
-                let idsMovies = this.state.popularMovies.map(elemento => elemento.id)
+                let idsMovies = this.state.allMovies.map(elemento => elemento.id)
                 let movies = info.results.filter(elemento => !idsMovies.includes(elemento.id))
                 this.setState({
-                    cartelMovies: movies.slice(0, 10),
-                })
+                    moviesCar: [movies.slice(0, 5), movies.slice(5, 10)],
+                    cartelMovies: movies.slice(0, 5),
+                    indexCar: 0,
+                }, ()=>console.log(this.state.cartelMovies))
             })
     }
 
@@ -40,32 +49,62 @@ class Principal extends Component {
     }
 
     controlarCambios(e) {
-          this.setState({
+        this.setState({
             valor: e.target.value,
         }, () => {
-            if(e.target.value.length !== 0){
-            fetch('https://api.themoviedb.org/3/search/movie?api_key=' + this.state.key + '&query=' + this.state.valor)
-                .then(data => data.json())
-                .then(info => {
-                    this.setState({
-                        resultados: info.results
+            if (e.target.value.length !== 0) {
+                fetch('https://api.themoviedb.org/3/search/movie?api_key=' + this.state.key + '&query=' + this.state.valor)
+                    .then(data => data.json())
+                    .then(info => {
+                        this.setState({
+                            resultados: info.results
+                        })
                     })
-                })
             }
-        })         
-    }
-    
-    verMas() {
-        this.setState({
-            popularMovies: this.state.allMovies.slice(10,20)
         })
     }
 
+    verMasPop() {
+        this.setState({
+            popularMovies: this.state.movies[this.state.index + 1],
+            index: this.state.index + 1
+        })
+    }
+
+    verMenosPop() {
+        if(this.state.index !== 0){
+            this.setState({
+                popularMovies: this.state.movies[this.state.index - 1],
+                index: this.state.index - 1
+            })  
+        }
+        }
+
+        verMasCar() {
+            console.log(this.state.indexCar)
+            console.log(this.state.moviesCar[this.state.indexCar - 1])
+            this.setState({
+                cartelMovies: this.state.moviesCar[this.state.indexCar + 1],
+                indexCar: this.state.indexCar + 1
+            })
+        }
+    
+        verMenosCar() {
+            
+            if(this.state.indexCar !== 0){
+                this.setState({
+                    cartelMovies: this.state.moviesCar[this.state.indexCar - 1],
+                    indexCar: this.state.indexCar - 1
+                })  
+            }
+            }
+
     render() {
+        
         return (
             <div>
                 <form onSubmit={(e) => this.evitarSubmit(e)}>
-                    <input type="text" onChange={(e) => this.controlarCambios(e)} placeholder = '¿Qué querés ver?'/>
+                    <input type="text" onChange={(e) => this.controlarCambios(e)} placeholder='¿Qué querés ver?' />
                 </form>
 
                 {this.state.valor.length === 0 ?
@@ -73,21 +112,54 @@ class Principal extends Component {
                         <h1>Más Populares</h1>
                         <section className="movieContainer">
                             {this.state.popularMovies.map((elemento, i) => <MovieCard key={elemento + i} name={elemento.title} img={'https://image.tmdb.org/t/p/w342/' + elemento.poster_path} alt={elemento.title} description={elemento.overview} />)}
-                            <p onClick={()=>this.verMas()}>Ver más</p>
-                        </section>
 
+
+
+                        </section>
+                        <div className='flechas'>
+                            <p onClick={() => this.verMenosPop()}>
+                                <span className="material-symbols-outlined">
+                                    arrow_back_ios
+                                </span>
+                            </p>
+                            <p onClick={() => this.verMasPop()}>{this.state.index === 3 ? <Link to={'/'}>Ver todas</Link> :
+                                <span className="material-symbols-outlined">
+                                    arrow_forward_ios
+                                </span>
+                            }
+                            </p>
+                            
+                        </div>
                         <h1>En cartelera</h1>
                         <section className="movieContainer">
                             {this.state.cartelMovies.map((elemento, i) => <MovieCard key={elemento + i} name={elemento.title} img={'https://image.tmdb.org/t/p/w342/' + elemento.poster_path} alt={elemento.title} description={elemento.overview} />)}
                         </section>
+                        <div className='flechas'>
+                            <p onClick={() => this.verMenosCar()}>
+                                <span className="material-symbols-outlined">
+                                    arrow_back_ios
+                                </span>
+                            </p>
+                            <p onClick={() => this.verMasCar()}>{this.state.indexCar === 1 ? <Link to={'/'}>Ver todas</Link> :
+                                <span className="material-symbols-outlined">
+                                    arrow_forward_ios
+                                </span>
+                            }
+                            </p>
+                            
+                        </div>
                     </React.Fragment>
                     :
+        
                     <section className="movieContainer">
                         {this.state.resultados.map((elemento, i) => <MovieCard key={elemento + i} name={elemento.title} img={'https://image.tmdb.org/t/p/w342/' + elemento.poster_path} alt={elemento.title} description={elemento.overview} />)}
+                        
                     </section>
+                    
+
 
                 }
-           </div>
+            </div>
 
         )
     }
